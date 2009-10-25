afff3bcf9e71dae1c3cd14154c5d58e67135e88c
Nathan Wright <nathan@brandalism.com>
2009-10-24 18:55:09 -0600

    Typhoeus working.

--- digitalvision.rb
+++ digitalvision.rb
@@ -1,13 +1,14 @@
 require 'rubygems'
 require 'find'
 require 'digest/md5'
-require 'open-uri'
+# require 'open-uri'
+require 'typhoeus'
 require 'cgi'
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database => '/Users/nn/Desktop/digitalvision.sqlite3',
+  :database => '/Users/nn/projects/wrayco/digitalvision-patch/digitalvision.sqlite3',
   :pool => 5,
   :timeout => 5000
 )
@@ -16,26 +17,6 @@ end
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
   
-# 
-# i = 0
-# root = '/Users/nn/Downloads/digitalvision'
-# Find.find(root) do |path| 
-#   # if File.basename(path) =~ /file2$/
-#   #   puts "PRUNED #{path}"
-#   #   Find.prune
-#   # end
-#   if File.file?(path)
-#     local_hash = Digest::MD5.hexdigest(File.read(path))
-#     Item.create(:path => path.sub(root, ''), :local_hash => local_hash)
-#     i += 1
-#     break if i == 10
-#   end
-# end
-# 
-# puts "There are #{i} things"
-# # 13579 files
-
-
 class HackCheck
   def initialize(root_local_path, website)
     @root_local_path = root_local_path
@@ -43,8 +24,7 @@ class HackCheck
     @base_path = ''
     @filename = ''
     @item = nil
-    @ignore_files = ['.wmv', '.avi', '.mov', '.mp4', '.flv', '.mpg', '.jpg', '.tif', '.zip']
-    @ignore_files = @ignore_files.collect {|i| [i, i.upcase] }.flatten
+    @ignore_files = ['.wmv', '.avi', '.mov', '.mp4', '.flv', '.mpg', '.jpg', '.tif', '.zip'].collect {|f| [f, f.upcase] }.flatten
   end
   
   def check
@@ -59,36 +39,60 @@ class HackCheck
         @item = Item.find_by_path(@base_path)
         url = @website + @base_path
 
-        begin
-          file = open(url.gsub(/ /, '%20')) # open(CGI::escape(url))
-        rescue => e
-          file = nil
-          message = e.message
-        end
+        # begin
+          request = Remote.get(url.gsub(/ /, '%20')) # open(CGI::escape(url))
+          if request.code == 200
+            remote_hash = Digest::MD5.hexdigest(request.body)
+            if remote_hash == local_hash
+              state = 'synced'
+            else
+              state = 'different'
+            end
+
+            if @item
+              @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => state, :code => nil, :message => nil, :updated_at => Time.now)
+            else
+              @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :remote_hash => remote_hash, :state => state)
+            end
+          else
+            code = request.code
+            message = request.headers
+            if @item
+              remote_hash ||= nil
+              @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => 'unsynced', :code => code, :message => message, :updated_at => Time.now)
+            else
+              @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :code => code, :message => message)
+            end
+          end
+            
+        # rescue => e
+        #   file = nil
+        #   message = e.message
+        # end
         
-        if file
-          # can open file remotely
-          remote_hash = Digest::MD5.hexdigest(file.read)
-          if remote_hash == local_hash
-            state = 'synced'
-          else
-            state = 'different'
-          end
-          
-          if @item
-            @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => state, :message => nil, :updated_at => Time.now)
-          else
-            @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :remote_hash => remote_hash, :state => state)
-          end
-        else
-          # can't open file
-          if @item
-            remote_hash ||= nil
-            @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => 'unsynced', :message => message, :updated_at => Time.now)
-          else
-            @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :message => message)
-          end
-        end
+        # if file
+        #   # can open file remotely
+        #   remote_hash = Digest::MD5.hexdigest(file.body)
+        #   if remote_hash == local_hash
+        #     state = 'synced'
+        #   else
+        #     state = 'different'
+        #   end
+        #   
+        #   if @item
+        #     @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => state, :message => nil, :updated_at => Time.now)
+        #   else
+        #     @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :remote_hash => remote_hash, :state => state)
+        #   end
+        # else
+        #   # can't open file
+        #   if @item
+        #     remote_hash ||= nil
+        #     @item.update_attributes(:local_hash => local_hash, :remote_hash => remote_hash, :state => 'unsynced', :message => message, :updated_at => Time.now)
+        #   else
+        #     @item = Item.create(:filename => @filename, :path => @base_path, :local_hash => local_hash, :url => url, :message => message)
+        #   end
+        # end
         i += 1
       end
     end
@@ -103,11 +107,16 @@ class HackCheck
   end
 end
 
+class Remote
+  include Typhoeus
+end
+
 class CreateItems < ActiveRecord::Migration
   def self.up
     create_table :items do |t|
       t.string :filename, :null => false
       t.string :state, :default => 'unsynced'
+      t.integer :code
       t.string :message
       t.string :path, :null => false
       t.string :local_hash
